cb44812cf0ac253a8a0ac4c4fa442cbb6fa18664
Oleg195 <100124067+Oleg195@users.noreply.github.com>
2022-11-14 16:16:01 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './Todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-list';
+todos: Todo[]=[];
+newTodo:string;
+
+saveTodo(){
+  if(this.newTodo){
+let todo = new Todo();
+todo.name=this.newTodo;
+todo.isCompleted=true
+this.todos.push(todo);
+this.newTodo="";
+  }else{
+    alert("Plise Enter Todo");
+  }
+}
+done(id:number){
+  this.todos[id].isCompleted =!this.todos[id].isCompleted;
+}
+
+
 }
